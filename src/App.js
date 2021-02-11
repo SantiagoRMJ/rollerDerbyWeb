33a138a023e5b66159206e5e659073076ac17457
aosses <adrianoosses9@gmail.com>
2021-02-11 16:05:11 +0100

    feat: 404. botonera

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Equipos from './components/Equipos/Equipos';
 import Contacto from './components/Contacto/Contacto';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' exact component={Inicio} />
         <Route path='/equipos' exact component={Equipos} />
         <Route path='/contacto' exact component={Contacto} />
+        <Route component={NotFound} />
       </Switch>
     <Footer/>  
     </Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Header.scss'
+import '../../styles.scss'
 import NavBar from '../NavBar/NavBar'
 import logo from '../../assets/images/logo.png'
 
 export default function Header() {
     return (
         <>
-        <header className="Header">
-            <img className="Header_logo" src={logo} alt="Logo" />
-            <div className="Header_title">
-                <h1>ÚNETE A NOSOTROS</h1>
+        <header className="header">
+            {/*<img className="Header_logo" src={logo} alt="Logo" />*/}
+            <div className="header-main-image">
+            <h2>Únete a nosotros</h2>
+                {/*<h1>ÚNETE A NOSOTROS</h1>*/}
             </div>    
         </header>
         <NavBar/>   
